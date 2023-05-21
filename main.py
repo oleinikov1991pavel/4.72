bcd861b73c818c779e96499c501d80e2047b1ecd
oleinikov1991pavel <134002094+oleinikov1991pavel@users.noreply.github.com>
2023-05-21 22:17:02 +0300

    add file

---
+++ main.py
@@ -0,0 +1,11 @@
+number1 = int(input('Введите первое число\n'))
+number2 = int(input('Введите второе число\n'))
+number3 = int(input('Введите третье число\n'))
+
+if number1 % 2 == 0:
+    print(number1)
+    if number2 % 2 == 0:
+        print(number2)
+    if number3 % 2 == 0:
+        print(number3)
+
